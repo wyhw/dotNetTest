1e13e50956f8cfd9cdf41c1d8de198678afd2b85
wyhw <wyhw@msn.com>
2016-08-24 14:58:47 +0800

    test solution

---
+++ src/test/testweb/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using testLib;
+
+[assembly: OwinStartupAttribute(typeof(test.Startup))]
+namespace test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var cls1 = new Class1();
+            ConfigureAuth(app);
+        }
+    }
+}
