d803d23e7975ba93288c04b4460e3c1950bd9fa5
王卫华(wangweihua) <wyhw@users.noreply.github.com>
2016-09-13 12:18:00 +0800

    Update Global.asax.cs

--- src/test/testweb/Global.asax.cs
+++ src/test/testweb/Global.asax.cs
@@ -12,6 +12,7 @@ namespace test
     {
         protected void Application_Start()
         {
+            //Test
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
